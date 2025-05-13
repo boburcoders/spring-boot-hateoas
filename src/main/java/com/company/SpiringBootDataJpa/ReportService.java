3e753201282bc86a5b2ab43f3acb00c58c223e33
bobur <b.toshniyozov@wlv.ac.uk>
2025-05-13 11:20:42 +0500

    using async to create new thread to work faster

---
+++ src/main/java/com/company/SpiringBootDataJpa/ReportService.java
@@ -0,0 +1,46 @@
+package com.company.SpiringBootDataJpa;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+@Slf4j
+@Component
+public class ReportService {
+    ExecutorService executorService = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+
+    public void sendReportWithThreadJavaThread() {
+        Runnable runnable = () -> {
+            try {
+                TimeUnit.SECONDS.sleep(3);
+            } catch (Exception ignored) {
+            }
+            log.info("Report sending successful");
+        };
+//        executorService.submit(runnable);
+        CompletableFuture.runAsync(runnable);
+    }
+
+    @Async
+    public void sendReportWithAsync() {
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (Exception ignored) {
+        }
+        log.info("Report sending successful");
+        throw new RuntimeException("Runtime error");
+    }
+
+    public void sendReportWithoutNewThread() {
+        try {
+            TimeUnit.SECONDS.sleep(3);
+        } catch (Exception ignored) {
+        }
+        log.info("Report sending successful");
+    }
+}
--- src/main/java/com/company/SpiringBootDataJpa/SpiringBootDataJpaApplication.java
+++ src/main/java/com/company/SpiringBootDataJpa/SpiringBootDataJpaApplication.java
@@ -1,41 +1,20 @@
 package com.company.SpiringBootDataJpa;
 
-import com.company.SpiringBootDataJpa.models.Post;
-import com.company.SpiringBootDataJpa.repo.PostRepo;
-import com.fasterxml.jackson.core.type.TypeReference;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.boot.ApplicationRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.domain.Auditable;
-import org.springframework.data.domain.AuditorAware;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
-import java.net.URL;
-import java.util.List;
-import java.util.Optional;
 
 @SpringBootApplication
+@EnableAsync
 public class SpiringBootDataJpaApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpiringBootDataJpaApplication.class, args);
-
-
-    }
-
-    @Bean
-    ApplicationRunner runner(PostRepo postRepo, ObjectMapper objectMapper) {
-        return args -> {
-            URL url = new URL("https://jsonplaceholder.typicode.com/posts");
-            List<Post> postList = objectMapper.readValue(url, new TypeReference<>() {
-            });
-            postList.forEach(post -> post.setId(null));
-            postRepo.saveAll(postList);
-
-
-        };
 
     }
 
