4ecf46f184c8f3369b0b01d623c5a16ed25354f5
bobur <b.toshniyozov@wlv.ac.uk>
2025-05-16 10:54:55 +0500

    scheduling in spring boot using @Scheduling

--- src/main/java/com/company/SpiringBootDataJpa/SpiringBootDataJpaApplication.java
+++ src/main/java/com/company/SpiringBootDataJpa/SpiringBootDataJpaApplication.java
@@ -3,18 +3,33 @@ package com.company.SpiringBootDataJpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
-import org.springframework.core.task.TaskExecutor;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class SpiringBootDataJpaApplication {
 
     public static void main(String[] args) {
+
+        /*class MyScheduledTask extends TimerTask {
+            private final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+            @Override
+            public void run() {
+                System.out.println("\r" + "Time is: " + dateFormat.format(new Date()));
+            }
+
+        }
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new MyScheduledTask(), 4, 1);*/
         SpringApplication.run(SpiringBootDataJpaApplication.class, args);
 
     }
